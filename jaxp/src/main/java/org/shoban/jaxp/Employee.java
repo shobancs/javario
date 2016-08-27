2f31bb72080aeb58f642fca3f580dd77ccc03f7f
scheekur <scheekur@cisco.com>
2016-08-26 23:00:05 -0400

    resolved compilation error

---
+++ jaxp/src/main/java/org/shoban/jaxp/Employee.java
@@ -0,0 +1,10 @@
+package org.shoban.jaxp;
+
+public class Employee {
+
+	public String firstName;
+	public String lastName;
+	public String location;
+	public String id;
+
+}
---
+++ jndi/src/test/java/org/shoban/jndi/FilterArgs.java
@@ -0,0 +1,12 @@
+package org.shoban.jndi;
+
+import javax.naming.NamingEnumeration;
+
+public class FilterArgs {
+
+	public static void printSearchEnumeration(NamingEnumeration answer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
