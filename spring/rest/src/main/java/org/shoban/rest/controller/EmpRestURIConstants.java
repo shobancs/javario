8ffeb1eee736f005922fef4eb316dec5db9e729d
scheekur <scheekur@cisco.com>
2016-09-01 21:39:00 -0400

    Spring autowire rest upload

--- spring/rest/src/main/java/org/shoban/rest/controller/EmpRestURIConstants.java
+++ spring/rest/src/main/java/org/shoban/rest/controller/EmpRestURIConstants.java
@@ -2,9 +2,9 @@ package org.shoban.rest.controller;
 
 public class EmpRestURIConstants {
 
-	public static final String DUMMY_EMP = "/rest/emp/dummy";
-	public static final String GET_EMP = "/rest/emp/{id}";
-	public static final String GET_ALL_EMP = "/rest/emps";
-	public static final String CREATE_EMP = "/rest/emp/create";
-	public static final String DELETE_EMP = "/rest/emp/delete/{id}";
+	public static final String DUMMY_EMP = "/emp/dummy";
+	public static final String GET_EMP = "/emp/{id}";
+	public static final String GET_ALL_EMP = "/emps";
+	public static final String CREATE_EMP = "/emp/create";
+	public static final String DELETE_EMP = "/emp/delete/{id}";
 }
--- spring/springbeanlifecycle/src/main/java/org/shoban/springbeanlifecycle/main/SpringMain.java
+++ spring/springbeanlifecycle/src/main/java/org/shoban/springbeanlifecycle/main/SpringMain.java
@@ -13,7 +13,7 @@ public class SpringMain {
 		System.out.println("Spring Context initialized");
 		
 		MyEmployeeService service = ctx.getBean("myEmployeeService", MyEmployeeService.class);
-		//EmployeeService service = ctx.getBean("employeeService", EmployeeService.class);
+		EmployeeService service1 = ctx.getBean("employeeService", EmployeeService.class);
 
 		System.out.println("Bean retrieved from Spring Context");
 		
